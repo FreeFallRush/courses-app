458a4576506c5e1a36ff409d040140c3a77b52fb
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 17:36:42 +0200

    Added title, description, authorNames, duration to CourseCard component

--- src/components/Courses/components/CourseCard/CourseCard.tsx
+++ src/components/Courses/components/CourseCard/CourseCard.tsx
@@ -1,9 +1,16 @@
 import Button from "../../../../common/Button/Button";
+import { getCourseDuration } from "../../../../helpers/getCourseDuration";
 import { CourseCardProps } from "./CourseCard.types";
 
 function CourseCard({title, duration, creationDate, description authorNames}:CourseCardProps) {
     return (
         <div>
+            <h2>{title}</h2>
+            <div>
+                <p>{description}</p>
+                <p>{authorNames}</p>
+                <p>Duration: {getCourseDuration(duration)}</p>
+            </div>
             <Button buttonText="SHOW COURSE" />
         </div>
     );

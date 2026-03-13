3dad1fb20ea96be5adc8b8791f138227f496c1cc
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 17:25:21 +0200

    Imported types in CourseCard component

--- src/components/Courses/components/CourseCard/CourseCard.tsx
+++ src/components/Courses/components/CourseCard/CourseCard.tsx
@@ -1,6 +1,7 @@
 import Button from "../../../../common/Button/Button";
+import { CourseCardProps } from "./CourseCard.types";
 
-function CourseCard() {
+function CourseCard({title, duration, creationDate, description authorNames}:CourseCardProps) {
     return (
         <div>
             <Button buttonText="SHOW COURSE" />

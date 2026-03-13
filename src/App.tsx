46394316e774dd78a887f099f9f079ddbc29d0b9
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 17:16:23 +0200

    Imported mockedCoursesList and mockedAuthorsList from constants.ts in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header/Header";
+import Courses from "./components/Courses/Courses";
+import { mockedCoursesList, mockedAuthorsList } from "./constants";
+
 import "./App.css";
 
 function App() {
     return (
         <>
             <Header />
+            <Courses courses={mockedCoursesList} authors={mockedAuthorsList} />
         </>
     );
 }
--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,2 +1,25 @@
-function Courses() {}
+function Courses({ courses, authors }) {
+    return (
+        <div>
+            <p>test</p>
+            <div>
+                {courses.map((course) => {
+                    console.log(course);
+                    return (
+                        <div>
+                            <p>{course.title}</p>
+                            <p>{course.description}</p>
+                        </div>
+                    );
+                })}
+            </div>
+            <div>
+                {authors.map((author) => {
+                    console.log(author);
+                    return <p>{author.name}</p>;
+                })}
+            </div>
+        </div>
+    );
+}
 export default Courses;

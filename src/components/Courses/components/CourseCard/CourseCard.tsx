232a959d088c5d09e0115eaa470c9d450ec5c10b
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 16:33:12 +0200

    Added CourseCard component with button in CourseCard.tsx

---
+++ src/components/Courses/components/CourseCard/CourseCard.tsx
@@ -0,0 +1,11 @@
+import Button from "../../../../common/Button/Button";
+
+function CourseCard() {
+    return (
+        <div>
+            <Button buttonText="SHOW COURSE" />
+        </div>
+    );
+}
+
+export default CourseCard;

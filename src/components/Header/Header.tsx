2d78234b722f4de70efa6d5ebc86671d348e4968
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 15:47:54 +0200

    Added Header.module.css in Header folder
    Added header tag and styles in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import Logo from "./components/Logo/Logo";
 import Button from "../../common/Button/Button";
+import styles from "./Header.module.css";
+
 function Header() {
     return (
-        <>
+        <header className={styles.header}>
             <Logo />
             <Button buttonText="Logout" />
-        </>
+        </header>
     );
 }
 

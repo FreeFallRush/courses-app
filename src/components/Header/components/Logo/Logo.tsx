12234f3e9d1b85c0dfa3297c8dabfa764a8ddf73
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 15:26:09 +0200

    Added Logo.module.css in Logo folder
    Added styles for Logo component

--- src/components/Header/components/Logo/Logo.tsx
+++ src/components/Header/components/Logo/Logo.tsx
@@ -1,7 +1,8 @@
 import logo from "../../../../assets/logo.svg";
+import styles from './Logo.module.css'
 
 function Logo() {
-    return <img src={logo} alt="Logo" />;
+    return <img src={logo} alt="Logo" className={styles.logo} />;
 }
 
 export default Logo;

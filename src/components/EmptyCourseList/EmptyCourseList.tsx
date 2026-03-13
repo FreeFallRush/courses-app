8a76e03dd60f982e9a1f7390bc80cfb28669968c
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-13 18:38:55 +0200

    Added EmptyCourseList.module.css in EmptyCourseList folder
    Added styles to EmptyCourseList component

--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -1,12 +1,13 @@
 import Button from "../../common/Button/Button";
+import styles from "./EmptyCourseList.module.css";
 
 function EmptyCourseList() {
     return (
-        <>
+        <div className={styles.mainContainer}>
             <h2>Course List is Empty</h2>
             <p>Please use "Add New Course" button to add your first course</p>
             <Button buttonText="Add New Course" />
-        </>
+        </div>
     );
 }
 

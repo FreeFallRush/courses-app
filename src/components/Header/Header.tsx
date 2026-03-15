0833cb1b434249a1e995697460fe3ede7e6a8058
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-15 20:50:53 +0200

    Added typescript types, showLogout and userName to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import Logo from "./components/Logo/Logo";
 import Button from "../../common/Button/Button";
+import { HeaderProps } from "./Header.types";
 import styles from "./Header.module.css";
 
-function Header() {
+function Header({ showLogout = true, userName }: HeaderProps) {
     return (
         <header className={styles.header}>
             <Logo />
+            {showLogout && <div>{userName && <span>{userName}</span>}</div>}
             <Button buttonText="Logout" />
         </header>
     );

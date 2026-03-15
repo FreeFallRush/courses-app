19b787469e2da79c03816a8f2cdb8eaaf3799ee1
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-15 20:52:08 +0200

    Deleted Header and CreateCourse from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import Header from "./components/Header/Header";
 import Courses from "./components/Courses/Courses";
 import { mockedCoursesList, mockedAuthorsList } from "./constants";
-import CreateCourse from "./components/CreateCourse/CreateCourse";
 
 import "./App.css";
 
 function App() {
     return (
         <>
-            <CreateCourse />
-            <Header />
             <Courses courses={mockedCoursesList} authors={mockedAuthorsList} />
         </>
     );

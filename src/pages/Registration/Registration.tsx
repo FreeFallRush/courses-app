ef380e180c474609e5bbc2ffa98dcede90d5da99
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-15 14:30:14 +0200

    Imported AuthForm component in Registration.tsx

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,25 +1,22 @@
 import { useState } from "react";
-import Input from "../../common/Input/Input";
-import Button from "../../common/Button/Button";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 function Registration() {
     const [name, setName] = useState("");
 
     return (
         <div>
-            <h3>Registration</h3>
-            <form>
-                <Input
-                    labelText="Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholderText="Input text"
-                />
-                <Button buttonText="Login" />
-                <p>
-                    If you have an account you may <strong>Login</strong>
-                </p>
-            </form>
+            <AuthForm
+                title="Registration"
+                fields={[
+                    {
+                        label: "Name",
+                        name: "name",
+                        value: name,
+                        onChange: setName,
+                    },
+                ]}
+            />
         </div>
     );
 }

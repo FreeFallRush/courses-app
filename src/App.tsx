8b85abfdb5945fe7220eb98ac3f6007ddbb49026
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-15 16:57:20 +0200

    Added Login component in Login.tsx
    Imported Login component in App.tsx to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header/Header";
 import Courses from "./components/Courses/Courses";
 import { mockedCoursesList, mockedAuthorsList } from "./constants";
-import Registration from "./pages/Registration/Registration";
+import Login from "./pages/Login/Login";
 
 import "./App.css";
 
 function App() {
     return (
         <>
-            <Registration />
+            <Login />
             <Header />
             <Courses courses={mockedCoursesList} authors={mockedAuthorsList} />
         </>
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import AuthForm from "../../components/AuthForm/AuthForm";
+import {
+    validateEmail,
+    validatePassword,
+} from "../../helpers/getFormValidation";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({ email: "", password: "" });
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const newErrors = {
+            email: validateEmail(email),
+            password: validatePassword(password),
+        };
+        setErrors(newErrors);
+
+        const hasErrors = Object.values(newErrors).some(
+            (error) => error !== ""
+        );
+        if (hasErrors) return;
+
+        console.log({ email: "", password: "" });
+    };
+
+    return (
+        <AuthForm
+            title="Login"
+            fields={[
+                {
+                    label: "Email",
+                    name: "email",
+                    value: email,
+                    onChange: setEmail,
+                    error: errors.email,
+                    type: "email",
+                },
+                {
+                    label: "Password",
+                    name: "password",
+                    value: password,
+                    onChange: setPassword,
+                    error: errors.password,
+                    type: "password",
+                },
+            ]}
+            onSubmit={handleSubmit}
+            submitButtonText="Login"
+            bottomText="If you don't have an account you may"
+            linkText="Registration"
+            onLinkClick={() => {}}
+        />
+    );
+}
+
+export default Login;

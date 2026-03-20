b4de42d9d57b629e23abed62193b0623fd356d85
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-20 22:47:52 +0200

    Changed Courses.tsx from props to redux store for courses and authors

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
+import {
+    getCourses as fetchCoursesFromApi,
+    getAuthors as fetchAuthorsFromApi,
+} from "../../services";
+
+import { setCourses } from "../../store/courses/actions";
+import { setAuthors } from "../../store/authors/actions";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+
 import CourseCard from "./components/CourseCard/CourseCard";
-import { CourseProps } from "./Courses.types";
 import Button from "../../common/Button/Button";
 import EmptyCourseList from "../EmptyCourseList/EmptyCourseList";
 import Header from "../Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
 import styles from "./Courses.module.css";
 
-function Courses({ courses, authors }: CourseProps) {
+function Courses() {
+    const dispatch = useAppDispatch();
+    const courses = useAppSelector((state) => state.courses);
+    const authors = useAppSelector((state) => state.authors);
+
     const [filteredCourses, setFilteredCourses] = useState(courses);
 
     const navigate = useNavigate();
@@ -17,6 +30,27 @@ function Courses({ courses, authors }: CourseProps) {
     for (const author of authors) {
         authorMap[author.id] = author.name;
     }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [coursesData, authorsData] = await Promise.all([
+                    fetchCoursesFromApi(),
+                    fetchAuthorsFromApi(),
+                ]);
+                dispatch(setCourses(coursesData));
+                dispatch(setAuthors(authorsData));
+                setFilteredCourses(coursesData);
+            } catch (error) {
+                console.error("Error fetching courses or authors", error);
+            }
+        };
+        fetchData();
+    }, [courses]);
+
+    useEffect(() => {
+        setFilteredCourses(courses);
+    }, [courses]);
 
     const getAuthors = (authorsIds: string[]): string[] => {
         const names: string[] = [];
--- src/services.ts
+++ src/services.ts
@@ -1,12 +1,12 @@
 const BASE_URL = "https://react-courses-app-1.onrender.com/api";
 
-export const getAllCourses = async () => {
+export const getCourses = async () => {
     const res = await fetch(`${BASE_URL}/courses/all`);
     if (!res.ok) throw new Error("Failed to fetch courses");
     return res.json();
 };
 
-export const getAllAuthors = async () => {
+export const getAuthors = async () => {
     const res = await fetch(`${BASE_URL}/authors/all`);
     if (!res.ok) throw new Error("Failed to fetch authors");
     return res.json();

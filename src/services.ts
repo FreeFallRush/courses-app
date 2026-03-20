b501e75699a1b1e5d7e2056f9cbd0c010f374131
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-20 20:54:19 +0200

    Added API calls for fetching courses and authors in services.ts

---
+++ src/services.ts
@@ -0,0 +1,13 @@
+const BASE_URL = "https://react-courses-app-1.onrender.com/api";
+
+export const getAllCourses = async () => {
+    const res = await fetch(`${BASE_URL}/courses/all`);
+    if (!res.ok) throw new Error("Failed to fetch courses");
+    return res.json();
+};
+
+export const getAllAuthors = async () => {
+    const res = await fetch(`${BASE_URL}/authors/all`);
+    if (!res.ok) throw new Error("Failed to fetch authors");
+    return res.json();
+};

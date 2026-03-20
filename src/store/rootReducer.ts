17067a5123aaad28f674237406e90149606bc6b1
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-20 18:39:51 +0200

    Added userReducer in rootReducer.ts

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authorsReducer from "./authors/reducer";
 import coursesReducer from "./courses/reducer";
+import userReducer from "./user/reducer";
 
 const rootReducer = combineReducers({
     authors: authorsReducer,
     courses: coursesReducer,
+    user: userReducer,
 });
 
 export default rootReducer;

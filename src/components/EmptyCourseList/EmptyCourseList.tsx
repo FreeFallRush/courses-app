40916e44102b00127145139c835ae5c4fffa4477
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-21 22:13:57 +0200

    Update EmptyCourseList to handle ADMIN and regular users

--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -1,21 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../store/hooks";
 import Button from "../../common/Button/Button";
 import styles from "./EmptyCourseList.module.css";
 
 function EmptyCourseList() {
     const navigate = useNavigate();
+    const user = useAppSelector((state) => state.user);
 
     const handleAddCourse = () => {
         navigate("/courses/add");
     };
 
+    const isAdmin = user.role?.toUpperCase() === "ADMIN";
+
     return (
         <div className={styles.mainContainer}>
             <h2>Course List is Empty</h2>
-            <p className={styles.para}>
-                Please use "Add New Course" button to add your first course
-            </p>
-            <Button buttonText="Add New Course" onClick={handleAddCourse} />
+            {isAdmin ? (
+                <>
+                    <p className={styles.para}>
+                        Please use "Add New Course" button to add your first
+                        course
+                    </p>
+                    <Button
+                        buttonText="Add New Course"
+                        onClick={handleAddCourse}
+                    />
+                </>
+            ) : (
+                <p className={styles.para}>
+                    You don't have permissions to create a course. Please log in
+                    as ADMIN
+                </p>
+            )}
         </div>
     );
 }

f0f66c700d0732e8e46ef03176bc13d2362512a9
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-21 21:29:46 +0200

    Fixed services.ts

--- src/services.ts
+++ src/services.ts
@@ -1,12 +1,24 @@
-const BASE_URL = "https://react-courses-app-1.onrender.com";
+import { Course } from "./store/courses/types";
+import { Author } from "./store/authors/types";
 
-export const getCourses = async () => {
+const BASE_URL = "https://react-courses-app-1.onrender.com";
+interface CoursesResponse {
+    successful: boolean;
+    result: Course[];
+}
+
+interface AuthorsResponse {
+    successful: boolean;
+    result: Author[];
+}
+
+export const getCourses = async (): Promise<CoursesResponse> => {
     const res = await fetch(`${BASE_URL}/courses/all`);
     if (!res.ok) throw new Error("Failed to fetch courses");
     return res.json();
 };
 
-export const getAuthors = async () => {
+export const getAuthors = async (): Promise<AuthorsResponse> => {
     const res = await fetch(`${BASE_URL}/authors/all`);
     if (!res.ok) throw new Error("Failed to fetch authors");
     return res.json();

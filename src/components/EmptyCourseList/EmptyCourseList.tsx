0880c0f2ba7bfbf6e1762c08d60944b2c9b848a4
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-21 12:42:17 +0200

    Added navigation to 'Add New Course' button in EmptyCourse.tsx

--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../common/Button/Button";
 import styles from "./EmptyCourseList.module.css";
 
 function EmptyCourseList() {
+    const navigate = useNavigate();
+
+    const handleAddCourse = () => {
+        navigate("/courses/add");
+    };
+
     return (
         <div className={styles.mainContainer}>
             <h2>Course List is Empty</h2>
             <p>Please use "Add New Course" button to add your first course</p>
-            <Button buttonText="Add New Course" />
+            <Button buttonText="Add New Course" onClick={handleAddCourse} />
         </div>
     );
 }

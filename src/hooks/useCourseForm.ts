e5d10819d50492f7c3cce16b2eb3e697005659c1
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-23 13:12:31 +0200

    Added CourseFormErrors type in useCourseForm.ts

--- src/hooks/useCourseForm.ts
+++ src/hooks/useCourseForm.ts
@@ -2,16 +2,17 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import formatCreationDate from "../helpers/formatCreationDate";
 import { validateCourseForm } from "../helpers/validateCourseForm";
+import { CourseFormErrors } from "../types/formErrors";
 
 export const useCourseForm = (
-    coursesAuthors: { id: string; name: string }[],
+    courseAuthors: { id: string; name: string }[],
     resetCourseAuthors: () => void
 ) => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [duration, setDuration] = useState("");
     const [authorName, setAuthorName] = useState("");
-    const [errors, setErrors] = useState({
+    const [errors, setErrors] = useState<CourseFormErrors>({
         title: "",
         description: "",
         duration: "",
@@ -30,14 +31,13 @@ export const useCourseForm = (
         const creationDate = formatCreationDate(
             new Date().toLocaleDateString("en-US")
         );
-
         const newCourse = {
             id: uuidv4(),
             title: title.trim(),
             description: description.trim(),
             creationDate,
             duration: +duration,
-            authors: coursesAuthors.map((a) => a.id),
+            authors: courseAuthors.map((a) => a.id),
         };
 
         console.log("Created course:", newCourse);

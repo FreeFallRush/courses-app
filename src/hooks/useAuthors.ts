720e6929e814751a5516c5f8e0c56ef28b3f648d
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-23 13:18:15 +0200

    Simplifyed useAuthors hook and use shared Author type

--- src/hooks/useAuthors.ts
+++ src/hooks/useAuthors.ts
@@ -1,32 +1,21 @@
 import { useState } from "react";
-
-export type Author = { id: string; name: string };
+import { Author } from "../store/authors/types";
 
 export const useAuthors = () => {
-    const [authors, setAuthors] = useState<Author[]>([]);
     const [courseAuthors, setCourseAuthors] = useState<Author[]>([]);
 
     const addAuthor = (author: Author) => {
         setCourseAuthors((prev) => [...prev, author]);
-        setAuthors((prev) => prev.filter((a) => a.id !== author.id));
     };
 
     const deleteAuthor = (author: Author) => {
-        setAuthors((prev) => prev.filter((a) => a.id !== author.id));
         setCourseAuthors((prev) => prev.filter((a) => a.id !== author.id));
     };
 
-    const deleteAuthorFromList = (author: Author) => {
-        setAuthors((prev) => prev.filter((a) => a.id !== author.id));
-    };
-
     return {
-        authors,
-        setAuthors,
         courseAuthors,
         setCourseAuthors,
         addAuthor,
         deleteAuthor,
-        deleteAuthorFromList,
     };
 };

7dc60fe0edd1fee6a4f3dc747160229e627cb094
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-23 13:21:31 +0200

    Improved date parsing in formatCreationDate.ts

--- src/helpers/formatCreationDate.ts
+++ src/helpers/formatCreationDate.ts
@@ -1,5 +1,7 @@
 export default function formatCreationDate(dateStr?: string): string {
     if (!dateStr || typeof dateStr !== "string") return "Invalid date";
-    const [mounth, day, year] = dateStr.split("/");
-    return `${mounth}.${day}.${year}`;
+    const parts = dateStr.split(/[\/\.]/);
+    if (parts.length !== 3) return "Invalid date";
+    const [month, day, year] = parts;
+    return `${month}.${day}.${year}`;
 }
